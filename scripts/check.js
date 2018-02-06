bc8afd49af2995198f0756a57969b2edc00aed3a
Nicolai Schmid <nicolai@schmid.uno>
2018-02-07 00:17:28 +0100

    style(check): ran prettier on check.js file

--- scripts/check.js
+++ scripts/check.js
@@ -2,8 +2,11 @@ const shell = require('shelljs');
 const chalk = require('chalk');
 
 if (!shell.which('xelatex')) {
-    console.log((`Xelatex is ${chalk.red.bold('not')} installed, please check https://github.com/NicolaiSchmid/partum/#xelatex-installation if you're not shure how to install it`));
+    console.log(
+        `Xelatex is ${chalk.red.bold(
+            'not'
+        )} installed, please check https://github.com/NicolaiSchmid/partum/#xelatex-installation if you're not shure how to install it`
+    );
 } else {
     console.log(chalk.green(`You're system is all setup 👍`));
 }
-
