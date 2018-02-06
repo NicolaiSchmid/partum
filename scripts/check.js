f0ccde0602c301c9fe73e4c2458ea51d7049b509
Nicolai Schmid <nicolai@schmid.uno>
2018-02-06 23:18:39 +0100

    feat(check): switched from execa for xelatex checking to shelljs

--- scripts/check.js
+++ scripts/check.js
@@ -1,16 +1,9 @@
-const execa = require('execa');
+const shell = require('shelljs');
 const chalk = require('chalk');
 
-execa('xelatex', ['--version'], {
-    env: process.env,
-})
-    .then((result) => {
-        console.log(chalk.green(`You're system is all setup 👍`));
-    })
-    .catch((error) => {
-        if (error.code === 'ENOENT') {
-            console.log((`Xelatex is ${chalk.red.bold('not')} installed, please check https://github.com/NicolaiSchmid/partum/#xelatex-installation if you're not shure how to install it`));
-            return;
-        }
-        console.log(error);
-    });
+if (!shell.which('xelatex')) {
+    console.log((`Xelatex is ${chalk.red.bold('not')} installed, please check https://github.com/NicolaiSchmid/partum/#xelatex-installation if you're not shure how to install it`));
+} else {
+    console.log(chalk.green(`You're system is all setup 👍`));
+}
+
