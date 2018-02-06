f9e21ef781a2a1408a6ed856f9a8a51dcd0236e8
Nicolai Schmid <nicolai@schmid.uno>
2018-02-06 22:26:41 +0100

    feat: initial commit with skeleton

---
+++ bin/partum-scripts.js
@@ -0,0 +1,23 @@
+const script = process.argv.length > 1 ? process.argv[2] : undefined;
+
+switch (script) {
+    case 'lint':
+    case 'format':
+    case 'count':
+    case 'commit':
+    case 'test': {
+        require(`../scripts/${script}.js`);
+        break;
+    }
+    case '--version': {
+        const { version } = require('../package.json');
+        console.log(version);
+
+        break;
+    }
+    default: {
+        console.error(`Unknwon script "${script}".`);
+
+        process.exit(1);
+    }
+}
---
+++ config/paths.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const projectRoot = process.cwd();
+const selfRoot = path.resolve(__dirname, '..');
+
+const resolveProjectDirectory = (relativePath) => path.resolve(projectRoot, relativePath);
+const resolveSelfDirectory = (relativePath) => path.resolve(selfRoot, relativePath);
+
+module.exports = {
+    projectBuild: resolveProjectDirectory('build'),
+    projectIndexJs: resolveProjectDirectory('src/index.js'),
+    projectRoot,
+    projectSrc: resolveProjectDirectory('src'),
+
+    projectPrettier: resolveProjectDirectory('node_modules/.bin/prettier'),
+    projectMocha: resolveProjectDirectory('node_modules/.bin/mocha'),
+    projectNyc: resolveProjectDirectory('node_modules/.bin/nyc'),
+
+    selfPrettier: resolveSelfDirectory('node_modules/.bin/prettier'),
+    selfMocha: resolveSelfDirectory('node_modules/.bin/mocha'),
+    selfNyc: resolveSelfDirectory('node_modules/.bin/nyc'),
+    selfCommitizen: resolveSelfDirectory('node_modules/commitizen'),
+
+    selfESLintConfig: resolveSelfDirectory('config/.eslintrc.js'),
+    selfNodeModules: resolveSelfDirectory('node_modules'),
+    selfPrettierConfig: resolveSelfDirectory('config/.prettierrc.json'),
+    selfPrettierIgnore: resolveSelfDirectory('config/.prettierignore'),
+};
